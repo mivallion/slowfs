190c47ae791eda3812a9efece9d3b8d16c7a6c77
Viktor Sevastyanov <v.sevastyanov@vk.team>
2023-10-29 20:56:55 +0300

    YAML configuration files. HTTP API. TTL for every operation. Specify operation for every filepath

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2023, mivallion
 # Copyright (c) 2015, Nir Soffer
 # Copyright (c) 2013, Stavros Korokithakis
 # All rights reserved.
@@ -7,12 +8,12 @@
 from distutils.core import setup
 
 setup(
-    author="Nir Soffer",
-    author_email="nsoffer@redhat.com",
-    description=("A very slow file sysem for simulating overloded storage"),
+    author="mivallion",
+    author_email="mivallion@gmail.com",
+    description=("A very slow file sysem for simulating overloded storage with HTTP API control"),
     license="BSD",
     name="slowfs",
-    scripts=["slowfsctl", "slowfs"],
+    scripts=["slowfsctl", "slowfs", "slowfs_api"],
     url="https://github.com/nirs/slowfs",
-    version="0.1",
+    version="0.2",
 )
